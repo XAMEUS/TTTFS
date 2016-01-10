3672fde59cccad3fdb5c7b2f521b79f6aa490d51
Far <quentinpn11@gmail.com>
2016-01-10 10:45:49 +0100

    stdltfs.h update

--- stdltfs.h
+++ stdltfs.h
@@ -12,3 +12,8 @@ int is_free_file(disk_id id, uint32_t par, uint32_t free_file, uint32_t pos);
 
 error remove_file(disk_id id, uint32_t par, uint32_t pos);
 
+error add_blocks_file(disk_id id, uint32_t par, uint32_t file, uint32_t nblocks);
+
+error remove_blocks_file(disk_id id, uint32_t par, uint32_t file, uint32_t nblocks);
+
+error update_blocks_file(disk_id id, uint32_t par, uint32_t file, uint32_t next_size);
